34af0adde7b44f99b925b89e7385466941f0e63d
Nikolai Ehrhardt <n.ehrhardt88@gmail.com>
2024-01-07 22:16:19 +0100

    test examples

--- test/spock_pipeline_test.groovy
+++ test/spock_pipeline_test.groovy
@@ -3,18 +3,17 @@ import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
 
 public class JenkinsfileWithPropertySpec extends JenkinsPipelineSpecification {
 
-    Script base
 
-    def setup() {
-        base = loadPipelineScriptForTest("vars/base.groovy")
-    }
+
 
     def "base test"() {
         given:
-        def result = null
+        String result = null
+        Script base = loadPipelineScriptForTest("vars/base.groovy")s
 
         when:
         result = base.call()
+
         then:
         result == "hansi"
     }
